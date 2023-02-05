3c27ce6de5fdbf41be9047543280d5c325ab62a5
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-02-05 22:57:41 +0300

    input deposit and bet with input check done

--- black jack/black_jack.py
+++ black jack/black_jack.py
@@ -1,6 +1,14 @@
 # простой блек джек
 import random
 
+bet = 0
+bet_min = 1
+bet_max = 100
+deposit_max = 1000
+deposit_min = 100
+valid_deposit = [str(i) for i in range(deposit_min, deposit_max+1, 1)]
+valid_bet = [str(i) for i in range(bet_min, bet_max+1, 1)]
+deposit = 0
 fullStack = []
 dictCardandPoints = {2: '2♥,2♦,2♣,2♠',
                      3: '3♥,3♦,3♣,3♠',
@@ -21,7 +29,7 @@ dictCardNum = {1: '2♥', 2: '3♥', 3: '4♥', 4: '5♥', 5: '6♥',
                32: '7♣', 33: '8♣', 34: '9♣', 35: '10♣', 36: 'J♣', 37: 'D♣',
                38: 'K♣', 39: 'A♣', 40: '2♠', 41: '3♠', 42: '4♠', 43: '5♠', 44: '6♠',
                45: '7♠', 46: '8♠', 47: '9♠', 48: '10♠', 49: 'J♠', 50: 'D♠',
-               41: 'K♠', 52: 'A♠'}
+               51: 'K♠', 52: 'A♠'}
 
 
 # methods
@@ -33,30 +41,29 @@ def out_black(text):
     print("\033[30m\033[47m{}\033[0m".format(text))
 
 
-temprnd = 0
-tempRndstr = []
-
-
-def fillRandomStack():
+def fill_random_stack():
+    temp_random = 0
+    temp_rnd_str = []
     for k in range(0, 4):
-        tempRndstr.append(int(random.randrange(1, 53)))
+        temp_rnd_str.append(int(random.randrange(1, 53)))
         i = 0
         while i < 51:
-            temprnd = int(random.randrange(1, 53))
-            if temprnd in tempRndstr:
+            temp_random = int(random.randrange(1, 53))
+            if temp_random in temp_rnd_str:
                 i -= 1
             else:
-                tempRndstr.append(temprnd)
+                temp_rnd_str.append(temp_random)
             i += 1
-        fullStack.extend(tempRndstr)
-        tempRndstr.clear()
+        fullStack.extend(temp_rnd_str)
+        temp_rnd_str.clear()
 
 
-# cardHerts2 = '2    ♣ ♠ ♦ \n♥    \n  ♥  \n     \n     \n     \n     \n  ♥  \n    ♥\n     2'
-# out_red(' K♥ ')
-# out_black(' T♥ ')
-# deposit = int(input('Введите размер депозита'))
+while deposit not in valid_deposit:
+    deposit = input(f'Введите размер депозита от {deposit_min} до {deposit_max}: ')
+
 if not fullStack:
-    fillRandomStack()
+    fill_random_stack()
+while bet not in valid_bet:
+    bet = input(f'Введите вашу ставку от {bet_min} до {bet_max}: ')
 
-print(fullStack)
+print(f'Ставка [{bet}] принята!')
